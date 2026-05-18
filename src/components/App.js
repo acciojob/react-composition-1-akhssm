e769b5a6c2c81b3240ac7bfd51634ed8842f256a
Akshay <mandaakshay20@gmail.com>
2026-05-18 11:06:27 +0530

    Wrote required code

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,54 @@
+import React, { useState } from "react";
+import "./../styles/App.css";
 
-import React from "react";
-import './../styles/App.css';
+const Tabs = ({ tabs }) => {
+  const [activeTab, setActiveTab] = useState(0);
 
-const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* Tab Titles */}
+      <div className="tabs">
+        {tabs.map((tab, index) => (
+          <button
+            key={index}
+            onClick={() => setActiveTab(index)}
+            className={activeTab === index ? "active" : ""}
+          >
+            {tab.title}
+          </button>
+        ))}
+      </div>
 
-export default App
+      {/* Tab Content */}
+      <div className="tab-content">
+        {tabs[activeTab].content}
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  const tabsData = [
+    {
+      title: "Tab 1",
+      content: "This is the content of Tab 1",
+    },
+    {
+      title: "Tab 2",
+      content: "This is the content of Tab 2",
+    },
+    {
+      title: "Tab 3",
+      content: "This is the content of Tab 3",
+    },
+  ];
+
+  return (
+    <div>
+      {/* Do not remove the main div */}
+      <Tabs tabs={tabsData} />
+    </div>
+  );
+};
+
+export default App;
